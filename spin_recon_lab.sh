a782fdd333d333d5b66ae44c4eab3459978abcdd
cienfleur <redvelvet0214@protonmail.ch>
2026-04-28 20:37:00 +0100

    remove unused scripts

--- spin_recon_lab.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Spin up the recon_lab scenario using Vagrant with VMware Desktop provider
-SCENARIO=recon_lab vagrant up --provider=vmware_desktop "$@"
--- spin_recon_lab2.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Spin up the recon_lab scenario using Vagrant with VMware Desktop provider
-SCENARIO=recon_lab_demo vagrant up --provider=vmware_desktop "$@"
